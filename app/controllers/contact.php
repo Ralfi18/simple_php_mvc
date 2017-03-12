64062219f5f8dec1da7793d40dbf50b0827d3a3b
Rali <ralidimitrovdimitrov@gmail.com>
2017-03-12 16:24:15 +0200

    Initial commit

---
+++ app/controllers/contact.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Rali
+ * Date: 3/12/2017
+ * Time: 11:18 AM
+ */
+class Contact extends Controller
+{
+    public function index()
+    {
+        echo "Contact Index";
+    }
+}
